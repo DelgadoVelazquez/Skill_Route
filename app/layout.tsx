22d45bf3880e1d8dfb45d725aca5e076a3ee6d75
DelgadoVelazquez <20223tn121@utez.edu.mx>
2026-03-21 12:32:30 -0600

    arrglos con el encabezado

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AcceslyWrapper from "@/components/AcceslyWrapper";
-import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,7 +30,6 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         <AcceslyWrapper>
-          <Navbar />
           {children}
         </AcceslyWrapper>
       </body>
